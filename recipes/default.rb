a56e6583e44c1455266348424df16cf742d7207e
stellarsquall <stellarsquall@protonmail.ch>
2018-05-30 01:56:52 -0700

    functional execute resources for createdb and psql role creation

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,32 +6,26 @@
 
 apt_update 'update'
 
-package 'postgresql'
+package node['mydatabase']['postgres_package']
 
 service 'postgresql' do
 	action [ :start, :enable ]
 end
 
-user 'database_user'
-
-sudo 'admins' do
-  users 'database_user'
-end
-
-template '/etc/postgresql/9.5/main/pg_hba.conf' do
-  source 'pg_hba_ubuntu.erb'
+template node['mydatabase']['pg_hba'] do
+  source "pg_hba_#{node['platform']}.erb"
   variables(
   	database_user: 'database_user'
   )
   notifies :reload, 'service[postgresql]', :immediately
 end
 
-execute "createuser database_user --superuser" do
+execute "createdb roux" do
   user 'postgres'
-  not_if "psql -c '\\du' | grep database_user"
+  not_if "psql --dbname=roux"
 end
 
-execute "createdb roux" do
-  user 'database_user'
-  not_if "psql --dbname=roux"
+execute "psql roux -c \"CREATE ROLE database_user PASSWORD 'user_password' SUPERUSER LOGIN;\"" do
+	user 'postgres'
+	not_if "psql -c '\\du' | grep database_user"
 end
