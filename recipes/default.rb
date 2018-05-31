dd7cf368204ab0ef56a76bba26baefa284b251f8
stellarsquall <stellarsquall@protonmail.ch>
2018-05-30 20:23:20 -0700

    reduced clutter: create db, user template, add user role

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,17 +12,17 @@ service 'postgresql' do
 	action [ :start, :enable ]
 end
 
-template node['mydatabase']['pg_hba'] do
-  source "pg_hba_#{node['platform']}.erb"
+execute "createdb roux" do
+  user 'postgres'
+  not_if "psql --dbname=roux"
+end
+
+template '/etc/postgresql/9.5/main/pg_hba.conf' do
+  source "pg_hba.erb"
   variables(
   	database_user: 'database_user'
   )
   notifies :reload, 'service[postgresql]', :immediately
-end
-
-execute "createdb roux" do
-  user 'postgres'
-  not_if "psql --dbname=roux"
 end
 
 execute "psql roux -c \"CREATE ROLE database_user PASSWORD 'user_password' SUPERUSER LOGIN;\"" do
